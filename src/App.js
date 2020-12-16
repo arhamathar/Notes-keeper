2c875c77c8a56b13b4f5f82bc159d347baec0db1
arhamathar <arhamathar789@gmail.com>
2020-12-16 16:53:23 +0530

    UI of the keeper app complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import CreateArea from './components/CreateArea';
+import Note from './components/Note';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <CreateArea />
+            <Note key={1} title="Note title" content="Note content" />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
